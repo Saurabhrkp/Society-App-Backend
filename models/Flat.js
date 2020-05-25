2eafd3265c27d484e78261dc2cd636a03ee7de0a
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2020-05-25 22:03:52 +0530

    Saving Flats as a entity

--- models/Flat.js
+++ models/Flat.js
@@ -7,47 +7,15 @@ const FlatSchema = new Schema({
   flatno: { type: Number, required: true },
   name: { type: String, required: true },
   description: { type: String, required: true },
-  ofRecord: { type: Schema.ObjectId, ref: 'Record', required: false },
-  maintenance: {
-    amount: { type: Number, required: true, default: 0 },
-    penality: { type: Number, required: true, default: 0 },
-  },
-  shedMoney: {
-    amount: { type: Number, required: true, default: 0 },
-    penality: { type: Number, required: true, default: 0 },
-  },
-  liftFund: {
-    amount: { type: Number, required: true, default: 0 },
-    penality: { type: Number, required: true, default: 0 },
-  },
-  convenance: {
-    amount: { type: Number, required: true, default: 0 },
-  },
-  sinkRepair: {
-    amount: { type: Number, required: true, default: 0 },
-  },
-  finalAmount: {
-    amount: { type: Number, required: true, default: 0 },
-  },
+  records: [{ type: Schema.ObjectId, ref: 'Record', required: false }],
 });
 
-const calculateTotalBy = function (next) {
-  this.finalAmount =
-    +this.maintenance.amount +
-    this.maintenance.penality +
-    this.shedMoney.amount +
-    this.shedMoney.penality +
-    this.liftFund.amount +
-    this.liftFund.penality +
-    this.convenance.amount +
-    this.sinkRepair.amount;
+const autoPopulateUserBy = function (next) {
+  this.populate('records');
   next();
 };
 
-FlatSchema.pre('save', calculateTotalBy);
-
-/* Create index on keys for more performant querying/Flat sorting */
-FlatSchema.index({ publishedDate: 1 });
+FlatSchema.pre('findOne', autoPopulateUserBy).pre('find', autoPopulateUserBy);
 
 /* The MongoDBErrorHandler plugin gives us a better 'unique' error, rather than: "11000 duplicate key" */
 FlatSchema.plugin(mongodbErrorHandler);
